89105b2e82b10339f66c840f66eceabb884996eb
Justica <atsuronreplaced@gmail.com>
2022-04-09 11:24:53 +0900

    Add viewmodels

---
+++ Strategic/Game/Fields/Base/IFieldCell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Fields.Base
+{
+    public interface IFieldCell
+    {
+        public uint XCoord { get; init; }
+        public uint YCoord { get; init; }
+
+        string GetColor();
+    }
+}
---
+++ Strategic/Strategic/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace App.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
