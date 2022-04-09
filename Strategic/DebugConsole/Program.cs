5b14e6885f0cdc2bbc02ed825e132e07dc0d32e5
Justica <atsuronreplaced@gmail.com>
2022-04-09 19:58:52 +0900

    Add new project to console debug

---
+++ Strategic/DebugConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Game.Units;
+
+namespace DebugConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(String.Equals(UnitTypes.Infantry.ToString(), "Infantry"));
+        }
+    }
+}
