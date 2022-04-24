6b3d5e70edc83ec2829886a368a3d6e57edad7f0
atsuhiron <atsuronreplaced@gmail.com>
2022-04-24 14:06:27 +0900

    [WIP] Add load command

---
+++ Strategic/Strategic/Commands/IO/Base/BaseIOCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using App.Constants;
+
+namespace App.Commands.IO.Base
+{
+    public abstract class BaseIOCommand : ICommand
+    {
+        protected string DefaultPath => Paths.saveDataDir;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public abstract bool CanExecute(object? parameter);
+        public abstract void Execute(object? parameter);
+    }
+}
---
+++ Strategic/Strategic/Commands/IO/LoadFileCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using App.Commands.IO.Base;
+
+namespace App.Commands.IO
+{
+    public class LoadFileCommand : BaseIOCommand
+    {
+        public override bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string ExecuteOpenFileDialog()
+        {
+            var dialog = new OpenFileDialog { InitialDirectory = DefaultPath };
+            dialog.ShowDialog();
+
+            return dialog.FileName;
+        }
+        //参考文献：
+        //https://docs.microsoft.com/ja-jp/dotnet/desktop/wpf/windows/how-to-open-common-system-dialog-box?view=netdesktop-6.0
+        //https://code-examples.net/ja/q/18b631
+        //https://qiita.com/mikurisa/items/709d24e53b29c4839c67
+    }
+}
