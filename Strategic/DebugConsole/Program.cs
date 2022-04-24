44c946b5b1ef8bc01a37d403dc853de245708174
atsuhiron <atsuronreplaced@gmail.com>
2022-04-24 09:21:25 +0900

    Add method to serialize and deserialize BattleField data

--- Strategic/DebugConsole/Program.cs
+++ Strategic/DebugConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using Game.Units;
 
 namespace DebugConsole
@@ -9,6 +10,41 @@ namespace DebugConsole
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(String.Equals(UnitTypes.Infantry.ToString(), "Infantry"));
+            
+            var parent = new Parent();
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jstr = JsonSerializer.Serialize(parent, options);
+            Console.WriteLine(jstr);
+        }
+    }
+
+    internal class Sample
+    {
+        public string Name { get; set; }
+        public int Num { get; set; }
+
+        public Sample(string ss, int nn)
+        {
+            Name = ss;
+            Num = nn;
+        }
+    }
+
+    internal class Parent
+    {
+        public string ParentName { get; set; }
+        
+        public Sample[] Samples { get; set; }
+
+        public Parent()
+        {
+            ParentName = "pare";
+            Samples = new Sample[]
+            {
+                new Sample("alpha", 23),
+                new Sample("bravo", 43),
+                new Sample("charie", 18)
+            };
         }
     }
 }
---
+++ Strategic/Strategic/IO/SaveDataIO.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Game;
+using App.Constants;
+
+namespace App.IO
+{
+    public class SaveDataIO
+    {
+        public async static void Write(BattleField battleField, string fileName)
+        {
+            using FileStream createStream = File.Create(Paths.saveDataDir + fileName);
+            await JsonSerializer.SerializeAsync<BattleField>(createStream, battleField);
+            await createStream.DisposeAsync();
+        }
+
+        public async static Task<(bool, BattleField?)> Read(string fileName)
+        {
+            using FileStream openStream = File.OpenRead(Paths.saveDataDir + fileName);
+            BattleField? battleField = await JsonSerializer.DeserializeAsync<BattleField>(openStream);
+            return (battleField == null, battleField);
+        }
+    }
+}
